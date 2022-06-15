ef44357c3c853f623a2076c2b4a8fe20826e9579
Matheus-mc007 <100372036+Matheus-mc007@users.noreply.github.com>
2022-06-15 11:34:57 -0300

    Trabalho

---
+++ Exercicio1/src/Cep.java
@@ -0,0 +1,19 @@
+
+public class Cep extends Endereco {
+	
+	public Cep() {
+		super();
+	}
+
+	@Override
+	public String getCep() {
+		
+		return "Tipo Lgradouro Logradouro - Bairro, cidade - Estado, Cep";
+	}
+
+	
+	
+	
+	
+
+}
---
+++ Exercicio1/src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Endereco endereco = new Endereco();
+		endereco = new Cep();
+		
+		System.out.println(endereco.getCep());
+		
+		
+
+	}
+
+}
